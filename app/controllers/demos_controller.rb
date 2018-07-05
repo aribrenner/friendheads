29fcc773bf83ad6aaa4ae4acfafb3b32f3144e18
aribrenner <aribrenner@gmail.com>
2018-07-05 15:32:05 -0400

    move js to partials

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -16,58 +16,18 @@ class DemosController < ApplicationController
   def find_head_and_external_id
     @external_id = params[:head]
     @head = @external_id ? Head.find_by_external_id(@external_id) : Head.random_default
+    @head_id_message = head_id_message.html_safe
   end
 
   def set_ivars
-    @easy_setup = easy_setup
-    @master_setup = init_script_tag
-    @advanced_js = advanced_js
+    @easy_setup = render_html_string('easy_setup')
+    @master_setup = render_html_string('master_setup')
+    @advanced_js = render_js_string('advanced_setup')
+    @opacity_js = render_js_string('opacity')
+    @opacity_init_js = render_js_string('opacity_init')
+    @other_parent_js = render_js_string('other_parent')
+    @multi_heads_js = render_js_string('multi_heads')
     @head_id_message = head_id_message
-    @opacity_js = opacity_js
-    @opacity_init_js = opacity_init_js
-    @other_parent_js = other_parent_js
-    @multi_heads_js = multi_heads_js
-  end
-
-  def easy_setup
-    tag = <<-HTML.gsub(/^\s+/, "").chomp
-    #{init_script_tag}<script>#{easy_setup_js}</script>
-    HTML
-
-    tag.chomp
-  end
-
-  def easy_setup_js
-    <<-JAVASCRIPT.gsub( /^\s+/, "").chomp
-    document.addEventListener("DOMContentLoaded",function(){(new Friendheads('#{@head.external_id}')).add();});
-    JAVASCRIPT
-  end
-
-  def init_script_tag
-    <<-HTML.gsub( /^\s+/, "").chomp
-    <script src="http://www.friendheads.com/js/embed.js"></script>
-    HTML
-  end
-
-  def advanced_js
-    js = <<-JAVASCRIPT.gsub( /^\s+/, "")
-      var friendheads = new Friendheads('#{@head.external_id}'); // #{head_id_message}
-    JAVASCRIPT
-
-    js += "\n" + <<-JAVASCRIPT.gsub( /^\s+/, "")
-      friendheads.add(); // add one head
-      friendheads.add(7); // add 7 head
-      friendheads.add(-2); // remove 2 heads
-    JAVASCRIPT
-
-    js += "\n" + <<-JAVASCRIPT.gsub( /^\s+/, "")
-      friendheads.count(); // get head count
-      friendheads.count(5); // set head count
-    JAVASCRIPT
-
-    js += "\n" + <<-JAVASCRIPT.gsub( /^\s+/, "")
-      friendheads.destroy(); // remove instance from dom
-    JAVASCRIPT
   end
 
   def head_id_message
@@ -78,35 +38,17 @@ class DemosController < ApplicationController
     end
   end
 
-  def opacity_js
-    <<-JAVASCRIPT.gsub( /^\s+/, "")
-      friendheads.opacity(); // get opacity
-      friendheads.opacity(.6); // set opacity (some float from 0 to 1)
-    JAVASCRIPT
+  def render_html_string(name)
+    render_escaped_string(name, 'html')
   end
 
-  def opacity_init_js
-    <<-JAVASCRIPT.gsub( /^\s+/, "")
-      var options = { opacity: .7 };
-      var friendheads = new Friendheads('#{@head.external_id}', options);
-    JAVASCRIPT
+  def render_js_string(name)
+    render_escaped_string(name, 'js')
   end
 
-  def other_parent_js
-    <<-JAVASCRIPT.gsub( /^\s+/, "")
-      var element = document.getElementById('my-friendheads-container'); // positioned HTMLElement or jQuery object
-      var options = { container: element };
-      var friendheads = new Friendheads('#{@head.external_id}', options);
-    JAVASCRIPT
-  end
-
-  def multi_heads_js
-    <<-JAVASCRIPT.gsub( /^\s+/, "")
-      var ariHeads = new Friendheads('ari');
-      var meHeads = new Friendheads('#{@head.external_id}');
-      ariHeads.add(4);
-      meHeads.add(4);
-    JAVASCRIPT
+  def render_escaped_string(name, type)
+    str = render_to_string("demos/js/#{name}.#{type}.erb", layout: nil)
+    CGI.escapeHTML(str).html_safe
   end
 
 end
